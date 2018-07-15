a2cbcf61445c1ea527c622742f4a62a7d5715762
face4 <s1611361@u.tsukuba.ac.jp>
2018-07-15 11:20:58 +0900

    bug fix

--- Fermat.cpp
+++ Fermat.cpp
@@ -4,13 +4,32 @@ using namespace std;
 typedef long long ll;
 
 // a^b mod p を計算する
-ll calc(ll a, ll b, ll p){
+ll powerInMod(ll a, ll b, ll p){
     if(b == 0)  return 1;
 
     if(b % 2 == 0){
-        ll d = calc(a, b/2, p);
+        ll d = powerInMod(a, b/2, p);
         return (d*d) % p;
     }else{
-        return (a * calc(a, b-1, p)) % p;
+        return (a * powerInMod(a, b-1, p)) % p;
     }
+}
+
+// 未検証
+// 組み合わせ mod pを逆元を用いて高速に求める
+// pが素数である必要がある
+ll nCm(ll n, ll m, ll p){
+    ll ret = 1;
+    
+    // nCm == nC(n-m)
+    if(m > n/2) m = n-m;
+
+    for(ll i = 1; i <= m; i++){
+        ret *= n-(i-1);
+        ret %= p;
+        ret *= powerInMod(i, p-2, p);
+        ret %= p;
+    }
+    
+    return ret;
 }
