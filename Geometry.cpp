daff590e266562e06890a091e873a6805a8b18bb
face4 <s1611361@u.tsukuba.ac.jp>
2019-08-08 21:37:20 +0900

    update geometry.cpp

--- Geometry.cpp
+++ Geometry.cpp
@@ -3,6 +3,7 @@
 #include<iomanip>
 #include<cmath>
 #include<algorithm>
+#include<cassert>
 using namespace std;
 
 #define EPS (1e-10)
@@ -10,6 +11,8 @@ using namespace std;
 
 struct Point;
 typedef Point Vector;
+typedef vector<Point> Polygon;
+struct Circle;
 struct Segment;
 typedef Segment Line;
 double norm(Point a);
@@ -29,8 +32,16 @@ bool isParallel(Segment s1, Segment s2);
 int ccw(Point p0, Point p1, Point p2);
 bool intersect(Point p1, Point p2, Point p3, Point p4);
 bool intersect(Segment s1, Segment s2);
+bool intersect(Circle c, Line l); // ちゃんと検証はしてない
+
 Point project(Segment s, Point p);
 Point reflect(Segment s, Point p);
+Point getCrossPoint(Segment s1, Segment s2);
+pair<Point,Point> getCrossPoints(Circle c, Line l);
+
+double area(Polygon g); // convexでなくてもよい. absを取れば符号付き面積
+bool isConvex(Polygon g); // O(n^2) 線形時間アルゴリズムが存在するらしい
+int contains(Polygon g, Point p);
 
 struct Point{
     double x, y;
@@ -55,7 +66,13 @@ struct Point{
 };
 
 typedef Point Vector;
+typedef vector<Point> Polygon;
 
+struct Circle{
+    Point c;
+    double r;
+    Circle(Point c = Point(), double r = 0.0) : c(c), r(r) {}
+};
 
 struct Segment{
     Point p1, p2;
@@ -127,7 +144,6 @@ static const int CLOCKWISE = -1;
 static const int ONLINE_BACK = 2;   // p2->p0->p1
 static const int ONLINE_FRONT = -2; // p0->p1->p2
 static const int ON_SEGMENT = 0;    // p0->p2->p1
-
 int ccw(Point p0, Point p1, Point p2){
     Vector a = p1 - p0;
     Vector b = p2 - p0;
@@ -145,6 +161,9 @@ bool intersect(Point p1, Point p2, Point p3, Point p4){
 bool intersect(Segment s1, Segment s2){
     return intersect(s1.p1, s1.p2, s2.p1, s2.p2);
 }
+bool intersect(Circle c, Line l){
+    return getDistanceLP(l, c.c) < c.r+EPS;
+}
 
 Point project(Segment s, Point p){
     Vector base = s.p2 - s.p1;
@@ -156,17 +175,73 @@ Point reflect(Segment s, Point p){
     return p + (project(s, p) - p) * 2.0;
 }
 
+Point getCrossPoint(Segment s1, Segment s2){
+    Vector base = s2.p2 - s2.p1;
+    double d1 = abs(cross(base, s1.p1-s2.p1));
+    double d2 = abs(cross(base, s1.p2-s2.p1));
+    double t = d1 / (d1 + d2);
+    return s1.p1 + (s1.p2 - s1.p1) * t;
+}
 
+pair<Point,Point> getCrossPoints(Circle c, Line l){
+    assert(intersect(c, l));
+    Vector pr = project(l, c.c);
+    Vector e = (l.p2 - l.p1) / abs(l.p2 - l.p1);
+    double base = sqrt(c.r * c.r - norm(pr - c.c));
+    return make_pair(pr + e*base, pr - e*base);
+}
+
+double area(Polygon g){
+    int n = g.size();
+    Point o(0.0, 0.0);
+    double res = 0.0;
+    for(int i = 0; i < n; i++)  res += cross(g[i]-o, g[(i+1)%n]-o);
+    return abs(res) / 2.0;
+}
+
+bool isConvex(Polygon g){
+    bool ret = true;
+    int n = g.size();
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(cross(g[i]-g[(i+n-1)%n], g[j]-g[(i+n-1)%n]) < -EPS || cross(g[(i+1)%n]-g[i], g[j]-g[i]) < -EPS){
+                ret = false;
+            }
+        }
+    }
+    return ret;
+}
+
+static const int IN = 2;
+static const int ON = 1;
+static const int OUT = 0;
+int contains(Polygon g, Point p){
+    int n = g.size();
+    bool x = false;
+    for(int i = 0; i < n; i++){
+        Point a = g[i] - p, b = g[(i+1)%n] - p;
+        if(abs(cross(a, b)) < EPS && dot(a, b) < EPS)   return ON;
+        if(a.y > b.y)   swap(a, b);
+        if(a.y < EPS && EPS < b.y && cross(a, b) > EPS) x = !x;
+    }
+    return x ? 2 : 0;
+}
 
 int main(){
+    int n;
+    cin >> n;
+    Polygon g;
+    for(int i = 0; i < n; i++){
+        double x, y;
+        cin >> x >> y;
+        g.push_back(Point(x,y));
+    }
     int q;
     cin >> q;
-
     while(q-- > 0){
-        double x[8];
-        for(int i = 0; i < 8; i++)  cin >> x[i];
-        cout << intersect(Point(x[0],x[1]), Point(x[2],x[3]), 
-                          Point(x[4],x[5]), Point(x[6],x[7])) << endl;
+        double x, y;
+        cin >> x >> y;
+        cout << contains(g, Point(x,y)) << endl;
     }
     return 0;
 }
