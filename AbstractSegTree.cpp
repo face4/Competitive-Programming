ada56f7eda9467a26895aa6b315d5f18ab0f60fe
face4 <s1611361@u.tsukuba.ac.jp>
2020-03-06 10:37:28 +0900

    bug fix [AbstractSegTree]

--- AbstractSegTree.cpp
+++ AbstractSegTree.cpp
@@ -20,7 +20,7 @@ public:
     Seg(vector<T> v, T d, F f) : def(d), f(f){
         n = 1;
         while(n < v.size())  n *= 2;
-        node.resize(2*n-1);
+        node.resize(2*n-1, def);
         for(int i = 0; i < v.size(); i++)   node[n-1+i] = v[i];
         for(int i = n-2; i >= 0; i--)   node[i] = f(node[2*i+1], node[2*i+2]);
     }
@@ -36,13 +36,13 @@ public:
 
     T query(int a, int b){
         int L = a+n-1, R = b+n-1;
-        T ret = def;
+        T lx = def, rx = def;
         while(L < R){
-            if((R&1) == 0)  ret = f(ret, node[--R]);
-            if((L&1) == 0)  ret = f(ret, node[L++]);
+            if((R&1) == 0)  rx = f(node[--R], rx);
+            if((L&1) == 0)  lx = f(lx, node[L++]);
             L >>= 1, R >>= 1;
         }
-        return ret;
+        return f(lx, rx);
     }
 };
 
