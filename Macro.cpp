62229757dc6934e7599d66912ca8c96f0e513804
face4 <face4+face4@users.noreply.github.com>
2020-11-23 10:20:50 +0900

    change structure (revenge)

--- Macro.cpp
+++
@@ -1,4 +0,0 @@
-#define inRange(x, a, b) a <= x && x < b
-#define fcout(x) cout << fixed << setprecision(x)
-#define printb(x, a, b) if(x) cout << a << endl; else cout << b << endl
-
---
+++ Math/Modpow.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+typedef long long ll;
+// a^b mod p を計算する
+ll modpow(ll a, ll b, ll p = 1e9+7){
+    if(b == 0)  return 1;
+
+    if(b % 2 == 0){
+        ll d = modpow(a, b/2, p);
+        return (d*d) % p;
+    }else{
+        return (a%p * modpow(a, b-1, p)) % p;
+    }
+}
